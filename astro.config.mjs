5dc1e01ebb88d7118fce13fdf95cdf34181c269b
Peter Yates <peter@peteryates.net>
2023-07-28 09:47:31 +0000

    second

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import react from "@astrojs/react";
 import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
-
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
